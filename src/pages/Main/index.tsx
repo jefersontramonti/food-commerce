4eb06f1fb0f0b00fd6b697384670a4992eedf0d6
jefersontramonti <jefersontramonti@outlook.com>
2023-01-25 23:54:12 -0300

    Expansão do menu, Ajuste mobile

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles'
+import { Sidebar } from '../../components/Sidebar'
+
+export default function Main() {
+  return (
+    <Container>
+      <Sidebar />
+      <h1>Food Commerce</h1>
+    </Container>
+  )
+}
